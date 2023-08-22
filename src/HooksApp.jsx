255a74956210dbaae85316dd33238ac234fec927
Juan Botero <juandiegoboterorincon1994@gmail.com>
2023-08-22 18:50:09 -0500

    Contador React

---
+++ src/HooksApp.jsx
@@ -0,0 +1,12 @@
+import { ContadorComponent } from "./components/ContadorComponent"
+
+
+export const HooksApp = () => {
+    return (
+        <>
+            <h1> Aplicacion de Hooks </h1>
+            <hr />
+            <ContadorComponent />
+        </>
+    )
+}
